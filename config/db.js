830ee49d0efc2472eafdba7a6b65ba4868c6ce1e
meet <meetsodvadiya.crawlapps@gmail.com>
2024-08-27 12:23:00 +0530

    env removed..

--- config/db.js
+++ config/db.js
@@ -1,16 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            dbName: process.env.DATABASE_NAME
-        });
-        console.log('MongoDB connected');
-    } catch (error) {
-        console.error('MongoDB connection error:', error);
-        process.exit(1);
-    }
-}
+  try {
+    console.log(process.env.MONGO_URI);
+    await mongoose.connect("mongodb://localhost:27017/to-do", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      dbName: process.env.DATABASE_NAME,
+    });
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  }
+};
 
-module.exports = connectDB
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = process.env.PORT || 8000
-const taskRouter = require('./routes/task.router')
-const connectDB = require('./config/db');
-const dotenv = require('dotenv').config();
-const path = require('path');
-console.log(process.env.PORT)
+const port = process.env.PORT || 8000;
+const taskRouter = require("./routes/task.router");
+const connectDB = require("./config/db");
+const path = require("path");
 connectDB();
 app.use(express.json());
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-app.use('/api/task', taskRouter)
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/api/task", taskRouter);
 
 app.listen(port, () => {
-    console.log(`listening on ${port}`);
+  console.log(`listening on ${port}`);
 });
